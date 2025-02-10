32072782fa3145979c9a87199cb4f4f7e806ddce
mdrasel97 <mollarasel972@gmail.com>
2025-02-10 15:02:50 +0600

    loop task added

--- loop-task.js
+++ loop-task.js
@@ -1,2 +1,149 @@
 // task 1
 // "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+// let days = 1;
+// while(days <= 60){
+//     console.log(`invest at least 6 hrs every single day for next 60 days! ${days}`);
+//     days++;
+
+// }
+
+// for (let days = 1; days <= 60; days++){
+//     console.log(`invest at least 6 hrs every single day for next 60 days! ${days}`)
+// }
+
+
+// task 2
+// Find all the odd numbers from 61 to 100.
+// let oddNumber = 61;
+// while (oddNumber <=100){
+// if(oddNumber % 2 === 1){
+//     console.log(oddNumber);
+// }
+// oddNumber++;
+// }
+
+// for(let oddNumber = 61; oddNumber <= 100; oddNumber++){
+//     if(oddNumber % 2 === 1){
+//         console.log(oddNumber);
+//     }
+// }
+// console.log(oddNumber)
+
+
+// task 3
+// Find all the even numbers from 78 to 98.
+// let evenNumber = 78;
+// while (evenNumber <= 98){
+// if(evenNumber % 2 === 0){
+//     console.log(evenNumber);
+// }
+// evenNumber++;
+// }
+
+// for(let evenNumber = 78; evenNumber <= 98; evenNumber++){
+//     if(evenNumber % 2 === 0){
+//         console.log(evenNumber);
+//     }
+// }
+
+
+// task 4
+// Display sum of all the odd numbers from 81 to 131.
+
+// let oddNumber = 81;
+// let sum = 0;
+// // while(oddNumber <= 131){
+// //     sum = sum + oddNumber;
+// //     console.log(sum);
+// //     oddNumber+=2;
+// // }
+// // console.log('total sum', sum);
+
+// while(oddNumber <= 131){
+//     if(oddNumber % 2 === 1){
+//         sum = sum + oddNumber;
+//         console.log(sum)
+//     }
+//     oddNumber++;
+// }
+// console.log('total sum', sum)
+// 
+
+// task 5
+// Display sum of all the even numbers from 206 to 311.
+
+// let num = 206;
+// let sum = 0;
+// while(num <= 311){
+//     if(num % 2 === 0){
+//         sum = sum + num;
+//         console.log(sum);
+//     }
+//     num++;
+// }
+
+
+// let sum = 0;
+// for(let num = 206; num<=331; num++){
+//     if(num % 2 === 0){
+//         sum = sum + num;
+//         console.log(sum);
+//     }
+// }
+
+// task 6
+// namta 
+// let number = 5;
+// let i = 1;
+// while(i <= 10){
+//     console.log(`${number} * ${i} = ${number * i}`);
+//     i++;
+// }
+
+// let number = 4;
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${number} * ${i} = ${number * i}`)
+// }
+
+// task 7
+// break up 
+// Write a loop 1 to 200. Use break to exit the loop once you find 100.
+// let number = 1;
+// while(number <= 200){
+//     console.log(number);
+//     if(number >= 100){
+//         break;
+//     }
+//     number++;
+    
+// }
+
+// for(let number = 1; number <= 200; number++){
+//     console.log(number)
+//     if(number >= 100){
+//         break;
+//     }
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if ((i % 2) != 0) {
+//       console.log(i);
+//     }
+//   }
+
+
+// print even numbers
+// for (let i = 1; i <= 10; i++) {
+//     if ((i % 2) != 0) {
+//       continue;
+//     }
+//     console.log(i);
+//   }
+
+// for (let i = 1; i < 10; i++) {
+//     if (i % 2 == 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
