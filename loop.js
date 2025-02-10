dbb4c1cae3f5c20cd283a7a58dd80b1ba2d5d283
mdrasel97 <mollarasel972@gmail.com>
2025-02-10 09:39:49 +0600

    loop task added

---
+++ loop.js
@@ -0,0 +1,299 @@
+
+// recape js 
+// let num = 0;
+// const number = 10;
+
+// if(num >= 18){
+//     console.log('tomar biyar boyos hoiche')
+// }
+// else if(num <= 15){
+//     console.log('tomi lolli pop khao')
+// }
+// else{
+//     console.log('tomi pichhi')
+// }
+
+
+// let numbers = [20, 10, 50, 40];
+// for(const number of numbers){
+//     console.log(number);
+    
+// }
+
+// let fruits = ['mango', 'banana', 'apple', 'orange', 'goya kha'];
+// for(const fruit of fruits){
+//     console.log('i want to eat', fruit)
+// }
+
+
+// let count = 50;
+// while(count >= 10 && count <= 100){
+//     console.log(count);
+//     count++;
+// }
+
+
+// let count = 50;
+// while (count >= 10) {  
+//     console.log(count); 
+//     count++;
+// }
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num);
+//     num++;
+// }
+
+
+// let number = 1;
+// let sum = 0;
+// while(number <= 10){
+//     sum = sum + number;
+//     console.log(sum);
+//     number++;
+// }
+
+
+// let num = 1;
+// while(num <=10){
+    
+// if(num % 2 === 0){
+//     console.log('even number', num)
+// }
+
+//     num++;
+// }
+
+
+// let count = 11;
+// while(count <= 21 && count>=15){
+//     console.log(count);
+//     count++;
+
+// }
+
+
+// let number = 10;
+// let sum = 0;
+// while(number <= 100){
+//     if(number % 2 === 0){
+//         console.log('even number', number);
+//     }
+//     number++;
+
+    
+// }
+
+
+// let num = 50;
+// while(num <= 100){
+//     if(num % 2 ===1){
+//         console.log('odd number', num);
+//     }
+//     num++;
+// }
+
+// for(let i = 1; i <=5; i++){
+//     console.log(i);
+// }
+
+
+// let sum = 0;
+// for(num = 1; num <=20; num++){
+//     sum = sum + num;
+//     // console.log(num)
+// }
+
+// console.log('hello world', sum)
+
+
+// let number = 5;
+// let sum = 0;
+// while(number <=20){
+//     if(number % 2 ===0){
+//         sum = sum + number;
+        
+//     }
+//     number++;
+
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for(let number = 1; number <=10; number++){
+//     sum = sum + number;
+
+// }
+// console.log(sum);
+
+// let oddNumber = 1;
+// while(oddNumber <= 50){
+//     if(oddNumber % 2 === 1){
+
+//         console.log('this is odd number', oddNumber)
+//     }
+//     oddNumber++;
+    
+// }
+
+
+// for(let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+// let i = 10;
+// while(i >= 0){
+//     console.log(i);
+//     i--;
+// }
+
+// let num = 1;
+// while(num <=10){
+//     console.log(num);
+//     num++;
+// }
+
+// for(let num = 1; num <= 10; num++){
+//     console.log(num)
+// }
+
+// for(let emply = 1; emply >=1 && emply <=300; emply++){
+//     console.log(emply);
+// }
+
+
+// let evenNum = 1;
+// Sum = 0;
+// while(evenNum <=20){
+//     if(evenNum % 2 === 0){
+//         console.log('this is even number', evenNum)
+//     }
+//     evenNum++;
+
+// }
+// let sum = 0;
+// for(let evenNum = 1; evenNum <=20; evenNum++ ){
+//     if(evenNum % 2 === 0){
+//     sum = sum + evenNum;
+//     console.log(evenNum)
+
+//     }
+// }
+// console.log('even number', sum);
+
+
+// for(let num = 1; num <=20; num+=2){
+//     console.log(num)
+// }
+
+// let sum = 1;
+// while(sum <= 20){
+//     console.log(sum);
+//     sum+=2;
+// }
+
+// for(let num = 1; num <=30; num++){
+//     if(num % 5 ===0){
+//         console.log(num)
+//     }
+// }
+
+// let total = 0;
+// for(let num = 1; num <=30; num++){
+//     if(num % 3 ===0){
+//         console.log(num);
+//         total = total + num;
+//     }
+// }
+// console.log('this is total number', total)
+// let count = 6;
+// while (count > 5) {
+//     console.log(count);
+//     count++;
+// }
+
+
+// let num = 1;
+// while(num <=10){
+//     console.log(num);
+//     num++
+// }
+
+
+
+// let n = 10;
+// do{
+//     console.log(n);
+//     n++
+// }
+// while(n < 5);
+
+// let number = 1;
+// while(number <=10){
+//     console.log(number);
+//     number++;
+// }
+
+
+// for(let number = 1; number <= 10; number++){
+//     console.log(number);
+// }
+
+// const x = ""
+// if(!x.length){
+// console.log("Hey")
+// }else{
+// console.log("Hi")
+// }
+
+// let str = "Mississippi";
+// console.log(str.indexOf("i", 3));
+
+// let person = {};
+// person.name = "John";
+// person.age = 30;
+// console.log(person);
+// let str = "Hello";
+// str[0] = "h";
+// console.log(str);
+// const myObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+//    };
+   
+//    for (let prop in myObject) {
+//     console.log(myObject[prop]);
+//    }
+
+
+//    let num = 'rasel';
+//    let upp = num.toUpperCase()
+//    console.log()
+
+
+// let product = ["apple", "banana", "mango", "lichu", "apple", "mango"];
+// let u = [];
+// let i = 0;
+// while(i < product.length){
+//     let m = product[i];
+//     if(!u.includes(m)){
+//         u.push(m);
+
+//     }
+//     i++;
+// }
+// console.log(u);
+
+// let product = ["apple", "banana", "mango", "apple", "orange", "mango"];
+
+// let unique = [];
+// for(let i = 0; i < product.length; i++){
+//     const element = product[i];
+//     if( !unique.includes(element)){
+//         unique.push(element);
+
+//     }
+// }
+// console.log(unique);
